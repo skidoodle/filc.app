861ca9ce5a5fbf2984a6c6037bc2e3e9ddc0b81e
UnknownPlayer78 <nyitraiszabolcs4@gmail.com>
2020-08-31 13:43:37 +0200

    website 2.0

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Az új weboldal kódja
---
+++ docs/assets/js/downloads.js
@@ -0,0 +1,101 @@
+const fadeIn = (element, duration) => {
+  (function increment(value = 0) {
+    element.style.opacity = String(value);
+    if (element.style.opacity !== "1") {
+      setTimeout(() => {
+        increment(value + 0.1);
+      }, duration / 10);
+    }
+  })();
+};
+
+function getReleases() {
+  return new Promise((resolve, reject) => {
+    let container = document.getElementById("releasesContainer");
+    container.innerHTML = `
+      <h3 class="redhat mb-5" style="font-weight: 500;">Letöltések</h3>
+          <center>
+          <video src="../assets/img/loader.webm" autoplay="true" loop></video></center>`;
+    fetch("https://api.github.com/repos/filcnaplo/filcnaplo/releases")
+      .then((res) => res.json())
+      .then((res_json) => {
+        container.innerHTML = `
+          <h3 class="redhat mb-5" style = "font-weight: 500;" > Letöltések</h3>`;
+        resolve(res_json);
+      });
+  });
+}
+
+getReleases().then((releases) => {
+  let container = document.querySelector("#releasesContainer");
+  let counter = 0;
+
+  for (let release of releases) {
+    let release_date = new Date(release.published_at);
+    let compose = `
+                    <div class="release row redhat align-items-center mt-5 mb-5">
+                    <div class="col" style="white-space: nowrap;">
+                    <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">${
+      release.name
+      }</a></div>
+                    <div class="col">
+                    <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
+  
+                    <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
+                    
+                    <div class="col" style="white-space: nowrap;">
+                    <a href="${release.html_url}" class="github text-dark">
+                    <img src="../assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
+                    <span>GitHub</span></a>
+                    </div>
+
+                    <div class="col" >
+                    <a href="${
+      release.assets[0]
+        ? release.assets[0].browser_download_url
+        : "#"
+      }" class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft" style="white-space: nowrap; ${
+      release.assets[0] ? "" : "display: none !important"
+      }">
+                    <img src="../assets/img/icons/downloads.svg" alt="Download" class="mr-1">
+                    <span>Letöltés</span></a>
+                    </div>
+
+                        <div
+      class="modal fade"
+      id="modal${counter}"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ${release.body.replace(/\r\n/g, "<br />")}
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+    // append as a child
+    let releaseElement = document.createElement("div");
+    releaseElement.innerHTML = compose;
+    container.append(releaseElement);
+
+    counter++;
+  }
+
+  fadeIn(container, 350);
+});
