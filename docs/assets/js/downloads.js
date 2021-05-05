d809e017ee9fd13c59cfb1f935c0be215a746518
unknown <55922348+UnknownPlayer78@users.noreply.github.com>
2021-05-05 20:23:15 +0200

    platform downloads

--- docs/assets/js/downloads.js
+++ docs/assets/js/downloads.js
@@ -14,26 +14,26 @@ function getReleases() {
     let everythingContainer = document.querySelector("#everythingContainer");
     everythingContainer.innerHTML = `
     <center>
-          <video src="../assets/img/loader.webm" autoplay="true" loop></video></center>`
+          <video src="/assets/img/loader.webm" autoplay="true" loop></video></center>`
     fetch("https://api.github.com/repos/filcnaplo/filcnaplo/releases")
       .then((res) => res.json())
       .then((res_json) => {
         everythingContainer.innerHTML = `
         <div id="latestReleaseContainer" class="col w-md-75">
-    </div>
-    <div id="latestBetaContainer" class="col w-md-75">
-    </div>
-    <div id="releasesContainer" class="col w-md-75">
-    </div>`
+        </div>
+        <div id="latestBetaContainer" class="col w-md-75">
+        </div>
+        <div id="releasesContainer" class="col w-md-75">
+        </div>`
         let container = document.getElementById("releasesContainer");
         let latestPrereleaseContainer = document.querySelector("#latestBetaContainer");
         let latestReleaseContainer = document.querySelector("#latestReleaseContainer");
         container.innerHTML = `
           <h3 class="redhat mb-5" style = "font-weight: 500;" >Régebbi verziók</h3>`;
         latestReleaseContainer.innerHTML = `
-    <h3 class="redhat mb-5" style="font-weight: 500;">Legújabb verzió</h3>`;
+          <h3 class="redhat mb-5" style="font-weight: 500;">Legújabb verzió</h3>`;
         latestPrereleaseContainer.innerHTML = `
-    <h3 class="redhat mb-5" style="font-weight: 500;">Legújabb béta</h3>`;
+          <h3 class="redhat mb-5" style="font-weight: 500;">Legújabb béta</h3>`;
         resolve(res_json);
       });
   });
@@ -51,62 +51,78 @@ getReleases().then((releases) => {
   function addFirstRelease(release) {
     let release_date = new Date(release.published_at);
     let compose = `
-                    <div class="release row redhat align-items-center mt-5 mb-5">
-                    <div class="col" style="white-space: nowrap;">
+      <div class="release row redhat align-items-center mt-5 mb-3">
+        <div class="col" style="white-space: nowrap;">
 
-                    <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
-                      ${release.name}
-                    </a></div>
+        <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
+          ${release.name}
+        </a></div>
 
-                    <div class="col">
-                    <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
-  
-                    <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
-                    
-                    <div class="col" style="white-space: nowrap;">
-                    <a href="${release.html_url}" class="github text-dark">
-                    <img src="../assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
-                    <span>GitHub</span></a>
-                    </div>
+        <div class="col">
+        <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
 
-                    <div class="col download-count" style="white-space: nowrap;">
-                    <img src="../assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
-                    <span>${release.assets[0].download_count}</span>
-                    </div>
+        <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
+        
+        <div class="col" style="white-space: nowrap;">
+        <a href="${release.html_url}" class="github text-dark">
+        <img src="/assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
+        <span>GitHub</span></a>
+        </div>
 
-                    <div class="col" >
-                    <a href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
-                        class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft" 
-                        style="white-space: nowrap; ${release.assets[0] ? "" : "display: none !important"}">
-                    <img src="../assets/img/icons/download.svg" alt="Download" class="mr-1">
-                    <span>Letöltés</span></a>
-                    </div>
+        <div class="col download-count" style="white-space: nowrap;">
+        <img src="/assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
+        <span>${release.assets[0].download_count}</span>
+        </div>
 
-                        <div
-      class="modal fade"
-      id="modal${counter}"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ${release.body.replace(/\r\n/g, "<br />")}
+        <div
+          class="modal fade"
+          id="modal${counter}"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ${release.body.replace(/\r\n/g, "<br />")}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>`;
+
+      <div class="mb-5 mt-2 d-flex row" style="justify-content: center">
+        <a  href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
+            class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft mb-2" 
+            style="white-space: nowrap; ${release.assets[0] ? "display: block" : "display: none !important"}">
+          <img src="/assets/img/icons/download.svg" alt="Download" class="mr-1">
+          <span>Letöltés</span>
+        </a>
+        <a  href="https://testflight.apple.com/join/Ak6I1C4C"
+            target="_blank"
+            class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft mb-2"
+            style="white-space: nowrap;display:block;background-color:black">
+          <img src="/assets/img/icons/apple.svg" alt="App Store" class="mr-1 mb-1" height="24">
+          <span>App Store</span>
+        </a>
+        <a  href="fdroidrepos://filcnaplo.hu/fdroid/repo/"
+            target="_blank"
+            class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft mb-2"
+            style="white-space:nowrap;display:block;background-color:#295384">
+          <img src="/assets/img/icons/fdroid.svg" alt="FDroid" class="mr-1 mb-1" height="24">
+          <span>FDroid</span>
+        </a>
+      </div>`;
 
     // append as a child
     let releaseElement = document.createElement("div");
@@ -117,60 +133,60 @@ getReleases().then((releases) => {
   function addRelease(release) {
     let release_date = new Date(release.published_at);
     let compose = `
-                    <div class="release row redhat align-items-center mt-5 mb-5">
-                    <div class="col" style="white-space: nowrap;">
-                    <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
-                      ${release.name}
-                    </a></div>
-                    <div class="col">
-                    <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
-  
-                    <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
-                    
-                    <div class="col" style="white-space: nowrap;">
-                    <a href="${release.html_url}" class="github text-dark">
-                    <img src="../assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
-                    <span>GitHub</span></a>
-                    </div>
+      <div class="release row redhat align-items-center mt-5 mb-5">
+      <div class="col" style="white-space: nowrap;">
+      <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
+        ${release.name}
+      </a></div>
+      <div class="col">
+      <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
 
-                    <div class="col download-count" style="white-space: nowrap;">
-                    <img src="../assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
-                    <span>${release.assets[0].download_count}</span>
-                    </div>
+      <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
+      
+      <div class="col" style="white-space: nowrap;">
+      <a href="${release.html_url}" class="github text-dark">
+      <img src="/assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
+      <span>GitHub</span></a>
+      </div>
 
-                    <div class="col" >
-                    <a href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
-                       class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft" 
-                       style="white-space: nowrap; ${release.assets[0] ? "" : "display: none !important"}">
-                    <img src="../assets/img/icons/download.svg" alt="Download" class="mr-1">
-                    <span>Letöltés</span></a>
-                    </div>
+      <div class="col download-count" style="white-space: nowrap;">
+      <img src="/assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
+      <span>${release.assets[0].download_count}</span>
+      </div>
 
-                        <div
-      class="modal fade"
-      id="modal${counter}"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ${release.body.replace(/\r\n/g, "<br />")}
+      <div class="col" >
+      <a href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
+          class="btn downloadButtonSecondary text-white d-inline align-items-center mobileMarginLeft" 
+          style="white-space: nowrap; ${release.assets[0] ? "" : "display: none !important"}">
+      <img src="/assets/img/icons/download.svg" alt="Download" class="mr-1">
+      <span>Letöltés</span></a>
+      </div>
+
+      <div
+        class="modal fade"
+        id="modal${counter}"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ${release.body.replace(/\r\n/g, "<br />")}
+            </div>
           </div>
         </div>
-      </div>
-    </div>`;
+      </div>`;
 
     // append as a child
     let releaseElement = document.createElement("div");
@@ -180,62 +196,62 @@ getReleases().then((releases) => {
   function addPrerelease(release) {
     let release_date = new Date(release.published_at);
     let compose = `
-                    <div class="release row redhat align-items-center mt-5 mb-5">
-                    <div class="col" style="white-space: nowrap;">
-                    
-                    <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
-                      ${release.name}
-                    </a></div>
+      <div class="release row redhat align-items-center mt-5 mb-5">
+      <div class="col" style="white-space: nowrap;">
+      
+      <a class="version mt-0 mb-0 text-black"  data-toggle="modal" data-target="#modal${counter}">
+        ${release.name}
+      </a></div>
 
-                    <div class="col">
-                    <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
-  
-                    <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
-                    
-                    <div class="col" style="white-space: nowrap;">
-                    <a href="${release.html_url}" class="github text-dark">
-                    <img src="../assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
-                    <span>GitHub</span></a>
-                    </div>
+      <div class="col">
+      <p class="date mt-0 mb-0">${release_date.toLocaleDateString()}</p></div>
 
-                    <div class="col download-count" style="white-space: nowrap;">
-                    <img src="../assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
-                    <span>${release.assets[0].download_count}</span>
-                    </div>
+      <div class="col-md-3 mobilePaddingTop" style="padding-bottom:0"></div>
+      
+      <div class="col" style="white-space: nowrap;">
+      <a href="${release.html_url}" class="github text-dark">
+      <img src="/assets/img/icons/github-dark.svg" alt="Github" style="color:#212121" class="mr-1">
+      <span>GitHub</span></a>
+      </div>
 
-                    <div class="col" >
-                    <a href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
-                       class="btn downloadButton text-white d-inline align-items-center mobileMarginLeft" 
-                       style="white-space: nowrap; ${release.assets[0] ? "" : "display: none !important"}">
-                    <img src="../assets/img/icons/download.svg" alt="Download" class="mr-1">
-                    <span>Letöltés</span></a>
-                    </div>
+      <div class="col download-count" style="white-space: nowrap;">
+      <img src="/assets/img/icons/downloads.svg" alt="Letöltések" style="color:#212121" class="mr-1">
+      <span>${release.assets[0].download_count}</span>
+      </div>
 
-                        <div
-      class="modal fade"
-      id="modal${counter}"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ${release.body.replace(/\r\n/g, "<br />")}
+      <div class="col" >
+      <a href="${release.assets[0] ? release.assets[0].browser_download_url : "#"}" 
+          class="btn downloadButtonSecondary text-white d-inline align-items-center mobileMarginLeft" 
+          style="white-space: nowrap; ${release.assets[0] ? "" : "display: none !important"}">
+      <img src="/assets/img/icons/download.svg" alt="Download" class="mr-1">
+      <span>Letöltés</span></a>
+      </div>
+
+      <div
+        class="modal fade"
+        id="modal${counter}"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">${release.name}</h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ${release.body.replace(/\r\n/g, "<br />")}
+            </div>
           </div>
         </div>
-      </div>
-    </div>`;
+      </div>`;
 
     // append as a child
     let releaseElement = document.createElement("div");
@@ -243,6 +259,7 @@ getReleases().then((releases) => {
     latestPrereleaseContainer.append(releaseElement);
   }
 
+  // meh code
   for (let release of releases) {
     if (!addedFirst && !release.prerelease) {
       addedFirst = true;
