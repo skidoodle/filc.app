4113e86eac76e04a445b18e956f1ec49ab993ecd
55nknown <annon@annon.me>
2022-11-25 11:58:17 +0100

    add pages

---
+++ pages/downloads.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Downloads() {
+  return (
+    <>
+      <Head>
+        <meta
+          httpEquiv="refresh"
+          content="0; url=https://github.com/filc/naplo/releases/latest"
+        />
+      </Head>
+    </>
+  );
+}
