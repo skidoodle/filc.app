40e05856118e30b6a845d3dbcd2785b0b5d6cbfe
55nknown <annon@annon.me>
2022-11-25 08:56:12 +0100

    placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Hello, Filc!</h1>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Image
+        src={"/img/iphonecropped 2.png"}
+        alt={"Filc"}
+        width={584 / 2}
+        height={702 / 2}
+      />
+    </div>
+  );
 }
