8423c00782ec92e1e1585f132c6bc0927fa2dc03
55nknown <annon@annon.me>
2022-11-25 09:06:44 +0100

    meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Filc Napló</title>
+
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Egy nem hivatalos alkalmazás az eKréta rendszerhez"
+        />
+        <meta
+          name="keywords"
+          content="filc,naplo,filcnaplo,enaplo,e-naplo,napló,filc napló,e-napló,alkalmazás,kréta,ekréta,kreta,ekreta,diák,diak,szivacs"
+        />
+
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <Image
         src={"/img/iphonecropped 2.png"}
         alt={"Filc"}
         width={584 / 2}
         height={702 / 2}
       />
+      <b>Soon™️</b>
     </div>
   );
 }
